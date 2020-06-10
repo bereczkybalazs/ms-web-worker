665e7e13c33b16665060bbd246b21767afffd997
Balazs Bereczky <bereczkybalazs1@gmail.com>
2020-06-10 15:11:40 +0200

    add env and nvmrc

---
+++ src/components/WebWorkerSearch.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Ms. Web Worker Search</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WebWorkerSearch"
+    }
+</script>
+
+<style scoped>
+
+</style>
