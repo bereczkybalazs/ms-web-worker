00376ce53533898c92681c99644666f5a4c2ae12
Balazs Bereczky <bereczkybalazs1@gmail.com>
2020-06-15 22:24:46 +0200

    added tailwindcss

--- src/components/WebWorkerSearch.vue
+++ src/components/WebWorkerSearch.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>Ms. Web Worker Search</h1>
+    <div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/express-ssr-6e0e3.appspot.com/o/mr-user.jpg?alt=media&token=a3ed782d-df37-4496-8872-526cfc9ea802"
+        class="max-w-lg"
+      />
+    </div>
   </div>
 </template>
 
